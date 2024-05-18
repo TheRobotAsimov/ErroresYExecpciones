5f94997d370b4079bb42b123df7ca992f9f60a05
Isaias <th3r0b0t4s1m0v@gmail.com>
2024-05-17 18:30:01 -0600

    Unificacion de programas

--- EP1/src/ep1/EP1.java
+++ EP1/src/ep1/EP1.java
@@ -1,5 +1,7 @@
 package ep1;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 /*
@@ -19,23 +21,91 @@ import java.util.Scanner;
 public class EP1 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        
+        int option = -1; // Inicializar con un valor que no sea 0
+
+        do {
+            try {
+                // Mostrar el menú
+                System.out.println("Menú:");
+                System.out.println("1. Validación de printf");
+                System.out.println("2. Estructura de Control de archivo");
+                System.out.println("0. Salir");
+                System.out.print("Seleccione una opción: ");
+
+                // Leer la opción del usuario
+                option = scanner.nextInt();
+
+                // Ejecutar la opción seleccionada
+                switch (option) {
+                    case 1:
+                        System.out.println("Ha seleccionado la opción de Validación de printf.");
+                        validarPrintf();
+                        break;
+                    case 2:
+                        System.out.println("Ha seleccionado la opción de Estructura de Control de archivo.");
+                        estructuraControl();
+                        break;
+                    case 0:
+                        System.out.println("Saliendo del programa.");
+                        break;
+                    default:
+                        System.out.println("Opción no válida, por favor seleccione una opción válida.");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Error: Por favor ingrese un número entero.");
+                scanner.next(); // Limpiar el buffer del scanner
+            }
+
+            System.out.println(); // Línea en blanco para mejor presentación
+
+        } while (option != 0);
+    }
+    
+    public static void validarPrintf(){
+        Scanner scanner = new Scanner(System.in);
         String texto;
         Validaciones comi = new Validaciones();
         
-        //System.out.print("printf(");
-        texto = sc.nextLine();
-        if(texto.charAt(0) == 'ñ')
-        {
-            System.out.println("eeeeñeeeee");
-        }
-        System.out.println(texto);
-        /*try {
+        System.out.println("Escribe: ");
+        texto = scanner.nextLine();
+        
+        try {
             comi.validacion(texto);
             
         } catch (Exception e) {
             System.out.println(e.getMessage());
-        }*/
+            System.out.println("Problems");
+        }
     }
     
+    public static void estructuraControl(){
+        try {
+            analizarEstructura();
+        } catch (IOException ex) {
+            System.out.println("Problemas con archivo: "+ ex.getMessage());
+        } catch (Exception e){
+            System.out.println("Error: "+ e.getMessage());
+        }
+    }
+    
+    public static void analizarEstructura() throws IOException, Exception{
+        String nombreArchivo = "./EstructuraDeControl.txt";
+        Repositorio repo = new Repositorio(nombreArchivo);
+        Validaciones log = new Validaciones();
+        String textoArchivo;
+        
+        textoArchivo = repo.getTextoArchivo();
+            if(textoArchivo.contains("do")){
+                textoArchivo = textoArchivo.replaceAll("\\s", "");
+                log.validarEstructuraControl("Do-While", "do{}while();", textoArchivo);
+            } else if(textoArchivo.contains("while")){
+                textoArchivo = textoArchivo.replaceAll("\\s", "");
+                log.validarEstructuraControl("While", "while(){}", textoArchivo);
+            } else {
+                throw new Exception("Estructura no reconocida");
+            }
+    }
 }
--- EP1/src/ep1/Validaciones.java
+++ EP1/src/ep1/Validaciones.java
@@ -1,5 +1,8 @@
 package ep1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class Validaciones {
 
     public Validaciones() {
@@ -9,10 +12,8 @@ public final class Validaciones {
         validacionPrintf(texto);
         validacionParentesis1(texto);
         validacionComillas1(texto);
-        //validacionComillas2(texto);
+        validacionComillas2(texto);
         validacionFin(texto);
-        int numFormatos = contarFormatos(texto);
-        System.out.println(numFormatos);
         validarVariables(texto);
     }
 
@@ -43,23 +44,6 @@ public final class Validaciones {
         throw new Exception("Error de caracter \" al inicio de la sentencia");
     }
 
-public void validacionComillas2(String texto) throws Exception {
-    int i = texto.length() - 1; // Empieza desde la última posición de la cadena
-    while (i >= 0) {
-        char currentChar = texto.charAt(i);
-        if (currentChar == '"') {
-            // Verifica si la comilla no está seguida inmediatamente por un paréntesis de cierre
-            if (i + 1 < texto.length() && texto.charAt(i + 1) != ')') {
-                return; // La comilla es la última y no está seguida por un paréntesis de cierre
-            }
-        }
-        i--; // Avanza hacia la izquierda en la cadena
-    }
-    // Si llegamos aquí, significa que no se encontró ninguna comilla que cumpla con la condición
-    throw new Exception("Error de caracter \" al final de la sentencia");
-}
-
-
     public void validacionFin(String texto) throws Exception {
         if (texto.endsWith(");")) {
             return;
@@ -67,51 +51,108 @@ public void validacionComillas2(String texto) throws Exception {
         throw new Exception("Error de ; al finalizar la sentencia");
     }
 
-public int contarFormatos(String texto) throws Exception {
-    int i = 8; // Empieza después de 'printf("'
-    String aux = texto.substring(i);
-    int contador = 0;
-    int pos;
+    public int contarFormatos(String texto) throws Exception {
+        int i = 8; // Empieza después de 'printf("'
+        String aux = texto.substring(i);
+        int contador = 0;
+        int pos;
 
-    do {
-        pos = aux.indexOf('%');
-        if (pos != -1) {
-            if (pos + 1 < aux.length() && aux.substring(pos, pos + 2).matches("%[cdiouxefgansp]")) {
-                contador++;
-                aux = aux.substring(pos + 2); // Avanza después del formato válido
-            } else {
-                throw new Exception("El indicador de formato '%' no esta acompañado de un formato valido");
+        do {
+            pos = aux.indexOf('%');
+            if (pos != -1) {
+                if (pos + 1 < aux.length() && aux.substring(pos, pos + 2).matches("%[cdiouxefgansp]")) {
+                    contador++;
+                    aux = aux.substring(pos + 2); // Avanza después del formato válido
+                } else {
+                    throw new Exception("El indicador de formato '%' no esta acompañado de un formato valido");
+                }
+            }
+        } while (pos != -1);
+
+        return contador; // Devuelve el número de formatos encontrados
+    }
+
+    public void validarVariables(String texto) throws Exception {
+        int numFormatos = contarFormatos(texto);
+
+        // Encuentra la posición de la última comilla dentro del texto
+        int comillas = texto.lastIndexOf('"');
+
+        // Obtiene la parte después de la última comilla
+        String aux = texto.substring(comillas + 1).trim();
+        if (numFormatos > 0) {
+            // Si hay al menos un formato, verifica la presencia de coma y variables
+            if (!aux.contains(",")) {
+                throw new Exception("Falta coma despues de las ultimas comillas");
+            }
+
+            // Separa las variables por comas y cuenta
+            String[] variables = aux.split(",");
+            for(String s:variables){
+                System.out.println(s);
+            }
+            System.out.println(variables.length);
+            if (variables.length-1 != numFormatos) {
+                throw new Exception("Numero de variables no coincide con el numero de formatos");
             }
         }
-    } while (pos != -1);
-
-    return contador; // Devuelve el número de formatos encontrados
-}
-
-public void validarVariables(String texto) throws Exception {
-    int numFormatos = contarFormatos(texto);
-
-    // Encuentra la posición de la última comilla dentro del texto
-    int comillas = texto.lastIndexOf('"');
-
-    // Obtiene la parte después de la última comilla
-    String aux = texto.substring(comillas + 1).trim();
-    if (numFormatos > 0) {
-        // Si hay al menos un formato, verifica la presencia de coma y variables
-        if (!aux.contains(",")) {
-            throw new Exception("Falta coma despues de las ultimas comillas");
+    }
+    
+    public void validacionComillas2 (String texto) throws Exception{
+        texto = texto.replaceAll("\\s", "");
+        List<Integer> indices = busqueda(texto, '"');
+        
+        if(indices.size()%2 != 0){
+            throw new Exception("No se estan cerrando correctamente las comillas");
         }
-
-        // Separa las variables por comas y cuenta
-        String[] variables = aux.split(",");
-        for(String s:variables){
-            System.out.println(s);
+        if(texto.charAt(texto.lastIndexOf('"')+1) != ',' && texto.charAt(texto.lastIndexOf('"')+1) != ')'){
+            throw new Exception("Ultima comilla incorrecta.");
         }
-        System.out.println(variables.length);
-        if (variables.length-1 != numFormatos) {
-            throw new Exception("Numero de variables no coincide con el numero de formatos");
+        
+        Integer previo=0;
+        
+        for(Integer i:indices){
+            if(i != indices.getFirst() && i != indices.getLast()){
+                if(i != indices.get(1)){
+                    if(i != previo+1){
+                        throw new Exception("Comilla no se cierra correctamente");
+                    }
+                }
+                    previo = i;
+            }
+        }
+        
+        if(texto.charAt(texto.lastIndexOf('"')+1) == ',' && contarFormatos(texto)<1){
+            throw new Exception("Faltan formatos de argumentos");
         }
     }
-}
 
+    public List<Integer> busqueda(String texto, char simbolo){
+        List<Integer> lista = new ArrayList<>();
+
+        for(Integer i = 0; i < texto.length(); i++){
+            if(texto.charAt(i) == simbolo){
+                lista.add(i);
+            }
+        }
+
+        return lista;
+    }
+    
+    public void validarEstructuraControl(String nombreEstructura, String estructura, String textoArchivo) throws Exception{
+        if(textoArchivo.equals(estructura)){
+            System.out.println("Estructura " + nombreEstructura + " validada");
+        } else {
+            if(!textoArchivo.contains("(") || !textoArchivo.contains(")")){
+                throw new Exception("Estructura " + nombreEstructura + " NO valida. Le falta parentesis");
+            }
+            if(!textoArchivo.contains("{") || !textoArchivo.contains("}")){
+                throw new Exception("Estructura " + nombreEstructura + " NO valida. Le falta llaves");
+            }
+            if(textoArchivo.length() != estructura.length()){
+                throw new Exception("Estructura " + nombreEstructura + " NO valida. Tiene caracteres adicionales");
+            }
+            throw new Exception("Estructura " + nombreEstructura + " NO valida");
+        }
+    }
 }
